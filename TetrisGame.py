7a7495affe7049055fdf323bf8cf2f508ecd917c
Sven <sajad.var83@gmail.com>
2026-06-07 15:14:52 +0330

    small refactoring. get_occupied_cells() is now a seperate function

--- TetrisGame.py
+++ TetrisGame.py
@@ -52,34 +52,50 @@ L = [
 
 PIECES = [I, O, T, S, Z, J, L]
 
-def spawn_piece():  
+def spawn_piece(board):  #unfinished
     # random piece selection (later)
     # random initial rotation (later)
-    piece = 'T'
-    if piece == 'T':
-        return {
-            "shape": T,
-            "row": 0,
-            "col": 4
-        }
+
+    new_piece = {
+        "shape": T,
+        "row": 0,
+        "col": 4
+    }
+
+    return new_piece
+    
+
+def can_spawn(board, piece): # unfinished
+    # checking whether every occupied cell of piece is currently empty in board
+    return True #temp
+
+
+def get_occupied_cells(piece):
+
+    shape = piece["shape"]  # piece matrix (relative cordinates)
+    cells = []
+
+    # iteration on piece to find occupied cells (1's)
+    for r in range(len(shape)):
+        for c in range(len(shape[r])):
+            if shape[r][c] == 1:
+                # getting board-relative cords
+                board_row = piece["row"] + r
+                board_col = piece["col"] + c
+                cells.append((board_row, board_col))
+    
+    return cells
 
 
 def draw(board, piece):
 
     temp_board = [row[:] for row in board]
 
-    shape = piece["shape"]  # piece matrix (relative cordinates)
-    row_pos = piece["row"]
-    col_pos = piece["col"]
+    # getting occupied cells and marking them on temp_board
+    for row, col in get_occupied_cells(piece):
+        temp_board[row][col] = 1
 
-    # iteration on piece to find occupied cells (1's)
-    for r in range(len(shape)):
-        for c in range(len(shape[r])):
-            if shape[r][c] == 1:
-                # marking occupation on temp_board
-                temp_board[row_pos + r][col_pos + c] = 1
-
-    # printing board with occupied cells filled
+    # printing temp_board with occupied cells filled
     for row in temp_board:
         for cell in row:
             print("■" if cell!=0 else ".", end="")
@@ -90,70 +106,81 @@ def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
 
+def can_move(board, piece, direction):  # unfinished
+    pass
+
+
 def can_move_down(board, piece):
 
-    shape = piece["shape"]
+    for row, col in get_occupied_cells(piece):
+        target_row = row + 1
+        target_col = col
 
-    # iteration on piece to find occupied cells (1's)
-    for r in range(len(shape)):
-        for c in range(len(shape[r])):
-            if shape[r][c] == 1:
-                # converting piece-relative coordinates to board-coordinates 
-                board_row = piece["row"] + r
-                board_col = piece["col"] + c
-
-                # the cell underneath
-                target_row = board_row + 1
-                target_col = board_col 
-
-                # bottom surface
-                if target_row >= ROWS:
-                    return False
-                # existing blocks
-                if board[target_row][target_col] != 0:
-                    return False   
-    return True       
+        # bottom surface
+        if target_row >= ROWS:
+            return False
+        # existing blocks
+        if board[target_row][target_col] != 0:
+            return False
+        
+    return True
 
 
 def place_piece(board, piece):
-    
-    shape = piece["shape"]
+    # getting occupied cells & permenantly marking it on main board
+    for row, col in get_occupied_cells(piece):
+        board[row][col] = 1
 
-    # iteration on piece to find occupied cells
-    for r in range(len(shape)):
-        for c in range(len(shape[r])):
-            if shape[r][c] == 1:
-                # permenantly marking it on main board
-                board_row = piece["row"] + r
-                board_col = piece["col"] + c
-                board[board_row][board_col] = 1
+
+def clear_lines(board):    # unfinished
+    pass
+
+
+def rotate_piece(piece):   # unfinished
+    shape = piece["shape"]
+    rotated = list(zip(*shape[::-1]))  
+    return rotated 
 
 
 def gameloop():
 
     tetris = Game()
-    tetris.current_piece = spawn_piece()
+    tetris.current_piece = spawn_piece(tetris.board)
+    game_over = False
 
-    while True:
+    while not game_over:
+
         # drawing current state
         clear_screen()
         draw(tetris.board, tetris.current_piece)
         # support player moving the piece and re-drawing board before next frame (later)
 
         # ----- prepration for drawing next state -----
-
         if can_move_down(tetris.board, tetris.current_piece):
             tetris.current_piece["row"] += 1
         else:
             # permenantly place the piece in tetris.baord
             place_piece(tetris.board, tetris.current_piece)
             # spawn new piece
-            tetris.current_piece = spawn_piece()
-
+            new_piece = spawn_piece(tetris.board)
+            # game over if new piece immediately collides with older pieces
+            if can_spawn(tetris.board, new_piece):
+                tetris.current_piece = new_piece
+            else:
+                game_over = True
         # ---------------------------------------------
 
         time.sleep(0.15)
 
+    print(f"GAME OVER ! SCORE: {tetris.score}")
+
 
 if __name__ == "__main__":
     gameloop()
+
+
+
+
+"""
+make some functions use self for cleaner calls
+"""
